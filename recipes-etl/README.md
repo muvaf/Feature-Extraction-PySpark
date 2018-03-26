c170d7f1a4e39d9e5745685f6aaa4ab445035e03
Muvaffak Onus <onus.muvaffak@gmail.com>
2018-03-26 17:17:26 +0300

    Named arguments are added to run-pipeline. Readme is updated

--- recipes-etl/README.md
+++ recipes-etl/README.md
@@ -18,9 +18,8 @@ make production
 ./run-pipeline.sh
 ```
 Command above will run by default on local using all the cores available.
+You can use `--url` argument to pass a YARN master node url and `--input` argument to pass another data source.
+
+Keep in mind that the root directory when running is `dist` folder, so when you specify a data source, you either need to specify an absolute path or a path that is relative to `dist` folder
 
 Output will be on `dist/output/output_result.parquet`
-
-
-python3.5
-spark 1.6.0
--- recipes-etl/run-pipeline.sh
+++ recipes-etl/run-pipeline.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-MASTER_URL = "local[*]"
-DATA_SOURCE = "../data/recipes.json"
-if [ "$1" != "" ]; then
-    echo "Positional parameter 1 contains something"
-else
-    echo "Positional parameter 1 is empty"
-fi
+MASTER_URL="local[*]"
+DATA_SOURCE="../data/recipes.json"
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -u|--url)
+    MASTER_URL="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -i|--input)
+    DATA_SOURCE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+
 cd dist
-spark-submit --master local[*] --py-files jobs.zip,libs.zip main.py --job ProductionPipeline --job-args ../data/recipes.json
+spark-submit --master $MASTER_URL --py-files jobs.zip,libs.zip main.py --job ProductionPipeline --job-args $DATA_SOURCE
