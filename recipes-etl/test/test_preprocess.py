c574e904190a7eb8bac4c5e62c13f5a84a6deee4
Muvaffak Onus <onus.muvaffak@gmail.com>
2018-03-26 14:02:33 +0300

    Test added for filter keyword

--- recipes-etl/test/test_preprocess.py
+++ recipes-etl/test/test_preprocess.py
@@ -1,10 +1,17 @@
-from test.shared import PySparkTestCase
-from recipes.jobs import Preprocess
-import unittest
-class PreprocessTest(unittest.TestCase):
+from test.shared.PySparkTestCase import PySparkTestCase
+from recipes.jobs.Preprocess import *
+
+class PreprocessTest(PySparkTestCase):
 
     def test_filter_with_keyword(self):
-        self.assertTrue(True)
+        sample_df = self.sqlContext.read.json("test/data/sample-data.json")
+        filtered_df = filter_with_keyword(sample_df, "ingredients", "Chilies")
+        result_list = filtered_df.collect()
+        self.assertEqual(len(result_list), 3)
+        for row in result_list:
+            self.assertTrue(row.ingredients.find("Chilies") != -1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
